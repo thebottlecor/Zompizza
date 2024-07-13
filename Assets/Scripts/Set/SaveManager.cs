ea6a630f2c373121de0f212d3511755c8b2dd02f
thebottlecor <74951897+thebottlecor@users.noreply.github.com>
2024-07-13 16:50:03 +0900

    우주선 추가

--- Assets/Scripts/Set/SaveManager.cs
+++ Assets/Scripts/Set/SaveManager.cs
@@ -85,7 +85,7 @@ public struct Save2DArray<T>
 public class SaveManager : Singleton<SaveManager>
 {
 
-    public readonly int version = 11;
+    public readonly int version = 21;
 
 
     public override void CallAfterAwake()
